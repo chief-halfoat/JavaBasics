e93f61cddd0878c750cbf2f98168edcdd98972b3
chief-halfoat <johnathan.j.hart@gmail.com>
2022-02-17 19:14:26 -0500

    Updated fibonacci

--- src/group5/first/assignment/Task07.java
+++ src/group5/first/assignment/Task07.java
@@ -8,7 +8,7 @@ public class Task07 {
 		int n1=0, n2=1, n3;
  
 		System.out.print(n1+" "+n2);
-		for (int i=2; i<=10; i++) {
+		for (int i=2; i<10; i++) {
 			n3=n1+n2;
 			System.out.print(" "+n3);
 			n1=n2;
